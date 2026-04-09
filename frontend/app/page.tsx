2f7106dbb4ec5f17b691c624ee4b621e2d06bcaa
luann-bortoli <luanndebortoli@gmail.com>
2026-04-09 13:07:34 -0300

    feat/build:  add data-aos

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,30 +1,35 @@
+'use client'
+
 import Card from "@/components/card/Card";
 import styles from "./page.module.css";
 import DropdownCard from "@/components/dropdownCard/DropdownCard";
+import AOS from 'aos'
+import 'aos/dist/aos.css'
+import { useEffect } from "react";
 
 export default function Home() {
 
-  const featureCard = 
-  [
-    {
-    id: 1,
-    imgPath: "weapon.png",
-    title:"Wacky Weapons",
-    desc: "Choose from bazookas, grenades, water balloons, and more."
-  },
-  {
-    id: 2,
-    imgPath: "battle.png",
-    title:"Multiplayer Battles",
-    desc: "Challenge your friends and join global penguin tournaments."
-  },
-  {
-    id: 3,
-    imgPath: "customize.png",
-    title:"Customize Your Penguin",
-    desc: "Unlock outfits, accessories, and unique styles."
-  }
- ]
+  const featureCard =
+    [
+      {
+        id: 1,
+        imgPath: "weapon.png",
+        title: "Wacky Weapons",
+        desc: "Choose from bazookas, grenades, water balloons, and more."
+      },
+      {
+        id: 2,
+        imgPath: "battle.png",
+        title: "Multiplayer Battles",
+        desc: "Challenge your friends and join global penguin tournaments."
+      },
+      {
+        id: 3,
+        imgPath: "customize.png",
+        title: "Customize Your Penguin",
+        desc: "Unlock outfits, accessories, and unique styles."
+      }
+    ]
 
   const galleryCard = [
     {
@@ -64,9 +69,15 @@ export default function Home() {
     }
   ]
 
+  useEffect(() => {
+    AOS.init({
+      once: true
+    })
+  }, [])
+
   return (
     <>
-      <div className={styles.heroContainer}>
+      <div className={styles.heroContainer} id="home">
 
         <div className={styles.mainCard}>
           <a href="#play">
@@ -81,7 +92,7 @@ export default function Home() {
             <p className={styles.title}>Crazy Penguin Wars Revival</p>
           </div>
 
-          <p className={styles.desc} style={{margin: "0"}}>—</p>
+          <p className={styles.desc} style={{ margin: "0" }}>—</p>
 
           <p className={styles.desc}>The legendary penguin shooter is back with classic chaos, new updates, and multiplayer battles</p>
         </div>
@@ -89,38 +100,41 @@ export default function Home() {
 
       <div className={styles.featuresContainer}>
 
-        <p className={styles.sectionTitle}>Game Features</p>
+        <p className={styles.sectionTitle} data-aos="fade-up" data-aos-offset="200">Game Features</p>
 
         <div className={styles.cardGrid} id="features" >
 
           {featureCard.map((card) => {
             return (
-            <Card
-              key={card.id}
-              imgPath={card.imgPath}
-              title={card.title}
-              desc={card.desc}
-            />
+              <div key={card.id} data-aos="zoom-in" data-aos-delay={card.id * 100}>
+                <Card
+                  imgPath={card.imgPath}
+                  title={card.title}
+                  desc={card.desc}
+                />
+              </div>
             )
           })}
 
-      </div>
+        </div>
       </div>
 
       <div className={styles.galleryContainer} id="gallery" >
 
-        <p className={styles.sectionTitle}>Gallery</p>
+        <p className={styles.sectionTitle} data-aos="fade-up" data-aos-offset="250">Gallery</p>
 
         <div className={styles.cardGrid}>
 
-         {galleryCard.map((card) => {
-          return(
-            <Card
-            key={card.id} 
-            imgPath={card.imgPath}
-            />
-          )
-         })}
+          {galleryCard.map((card) => {
+            return (
+              <div key={card.id} data-aos="zoom-in" data-aos-delay={card.id * 100} data-aos-offset="200">
+                <Card
+                  imgPath={card.imgPath}
+                  data-aos="fade-up"
+                />
+              </div>
+            )
+          })}
 
         </div>
 
@@ -128,44 +142,45 @@ export default function Home() {
 
       <div className={styles.playContainer} id="play" >
 
-        <p className={styles.sectionTitle}>Play</p>
+        <p className={styles.sectionTitle} data-aos="fade-up" data-aos-offset="200">Play</p>
 
-        <p className={styles.sectionDesc}>Ready to dive into the icy battlefield? Click below to download the demo version.</p>
+        <p className={styles.sectionDesc} data-aos="fade-up" data-aos-offset="200">Ready to dive into the icy battlefield? Click below to download the demo version.</p>
 
-        <a download href="https://github.com/Crazy-Penguin-Wars/cpw-launcher/releases/download/1.0.0/Crazy.Penguin.Wars.1_0_0.zip">
+        <a download data-aos="zoom-in" href="https://github.com/Crazy-Penguin-Wars/cpw-launcher/releases/download/1.0.0/Crazy.Penguin.Wars.1_0_0.zip">
           Downlaod for Windowns
           <img src="download.svg" alt="" />
         </a>
 
-        <div className={styles.cardContainer}>
+        <div className={styles.cardContainer} data-aos="zoom-in" data-aos-delay="100">
           <video src="trailer.mp4" controls></video>
         </div>
 
       </div>
 
       <div className={styles.faqContainer} id="faq">
-        <div className={styles.containerTitle}>
+        <div className={styles.sectionTitle}>
           <p>FAQ</p>
         </div>
 
         <div className={styles.rowWrapper}>
           <div className={styles.faqGrid}>
 
-          {faqCard.map((card) => {
-            return(
-              <DropdownCard 
-                key={card.id}
-                title={card.title}
-                desc={card.desc}
-              />
-            )
-          })}
+            {faqCard.map((card) => {
+              return (
+                <div key={card.id} data-aos="zoom-in" data-aos-delay="100">
+                  <DropdownCard
+                    title={card.title}
+                    desc={card.desc}
+                  />
+                </div>
+              )
+            })}
 
-        </div>
+          </div>
 
-        <div className={styles.faqBanner}>
-          <img src="money.png" alt="" />
-        </div>
+          <div className={styles.faqBanner} data-aos="zoom-in" data-aos-delay="100">
+            <img src="money.png" alt="" />
+          </div>
         </div>
 
       </div>
